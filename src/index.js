b4eac900b2f34b102b82e9a7c0d7ab7dd3f6367f
jcmyplug <jcmyplug@outlook.com>
2026-05-19 01:35:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PlugApp from '../PlugMarketplace';
+import PlugApp from './PlugMarketplace';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<PlugApp />);
